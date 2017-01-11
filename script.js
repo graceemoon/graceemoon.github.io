28dd5faea4a11e7ed0da956f1072920ba9b8df33
grace moon <hi.gracemoon@gmail.com>
2017-01-11 16:55:41 -0500

    portfolio update

---
+++ script.js
@@ -0,0 +1,25 @@
+ //  document.addEventListener("DOMContentLoaded", function(event) {
+	// console.log("DOM loaded");
+ //  });
+console.log("DOM loaded");
+
+let navLink = document.querySelector('.links-butt');
+navLink.addEventListener("click", function(){
+	    alert('ho ho ho');
+});
+
+
+
+// susanna
+// $('#bubbleProjects').on('click', (event) => {
+//     $('body').animate({
+//       scrollTop: $('#bubbleProjects').offset().top -10
+//     }, 500);
+//     $('#modalProjects').css({'visibility': 'visible'});
+//     $('.bubble').css({
+//       width: '150px',
+//       height: '150px',
+//       fontSize: '20px',
+//       lineHeight: '100px',
+//     });
+//   });
